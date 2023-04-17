dd41e41f285427d4f95adb1d55e7fd843d30cb74
carlospuigserver <cpuiggut@myuax.com>
2023-04-17 18:24:23 +0200

    commit

--- ventanagrafica.py
+++ ventanagrafica.py
@@ -36,3 +36,41 @@ class Coche(threading.Thread):
         super().__init__()
         self.gasolinera=gasolinera
         self.ventana=ventana
+
+    def run(self):
+        tiempo_llegada=random.randint(0,15*100)
+        time.sleep=tiempo_llegada/100
+        print(f"Coche {self.name} llega a la gasolinera en el tiempo{time.time()} segundos.")
+        self.gasolinera.atender_coche()
+        print(f"Coche {self.name} sale de la gasolinera en el tiempo{time.time()} segundos.")
+        self.ventana.actualizar_surtidores()
+
+class Ventana:
+    def __init__(self,gasolinera):
+        self.gasolinera=gasolinera
+        self.root=tk.Tk()
+        self.root.title("Gasolinera")
+        self.surtidores_label=tk.Label(self.root,text=f"Surtidores libres: {gasolinera.num_surtidores}")
+        self.surtidores_label.pack()
+        self.llegada=tk.Button(self.root,text="Llegada de coche", command=self.llegada_coche)
+        self.llegada.pack()
+
+    def actualizar_surtidores(self):
+        self.surtidores_label.configure(text=f"Surtidores libres: {self.gasolinera.num_surtidores}")
+
+    
+    def llegada_coche(self):
+        coche=Coche(self.gasolinera,self)
+        coche.start()
+
+    def iniciar(self):
+        for i in range (50):
+            coche=Coche(self.gasolinera,self)
+            coche.start()
+        self.root.mainloop()
+
+if __name__ == "__main__":
+      gasolinera=Gasolinera(num_surtidores=1)
+      ventana=Ventana(gasolinera)
+      ventana.iniciar()
+  
